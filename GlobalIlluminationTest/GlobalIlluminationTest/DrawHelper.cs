ba045d7b64142ce75ad66c2b937848a89cedc9d7
wangtianhang <690879430@qq.com>
2018-05-05 23:25:59 +0800

    添加测试工程

---
+++ GlobalIlluminationTest/GlobalIlluminationTest/DrawHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class DrawHelper
+{
+    public static string GetTime()
+    {
+        DateTime now = DateTime.Now;
+        string ret = now.Year + "_" + now.Month + "_" + now.Day + "_" + now.Hour + "_" + now.Minute + "_" + now.Second;
+        return ret;
+    }
+
+    public static void DrawData(string filePath, int width, int height, Color[] data, string des)
+    {
+        Bitmap bitmap = DrawData(width, height, data);
+        using (Graphics g = Graphics.FromImage(bitmap))
+        {
+            using (Font font = new Font("宋体", 16f))
+            {
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+                g.DrawString(des, font, Brushes.Black, new Point(20, 20));
+            }
+
+        }
+        //string fileDir = Path.GetDirectoryName(filePath);
+        //string fileName = Path.GetFileNameWithoutExtension(filePath);
+        //bitmap.Save(fileDir + "/" + fileName + GetTime() + ".bmp");
+        bitmap.Save(filePath);
+    }
+
+    public static Bitmap DrawData(int width, int height, Color[] data)
+    {
+        Bitmap bitmap = new Bitmap(width, height);
+
+        for (int j = 0; j < height; ++j)
+        {
+            for (int i = 0; i < width; ++i)
+            {
+                int index = j * height + i;
+                bitmap.SetPixel(i, j, data[index]);
+            }
+        }
+        return bitmap;
+    }
+}
+
