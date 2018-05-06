501980bade2685f9a72216485219d598ea0fec49
wangtianhang <690879430@qq.com>
2018-05-06 17:22:21 +0800

    添加射线类

---
+++ GlobalIlluminationTest/GlobalIlluminationTest/Intersection/Ray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    struct Ray
+    {
+        Vector3 _origin;
+        Vector3 _direction;
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            _origin = origin;
+            _direction = direction;
+        }
+
+        public Vector3 origin 
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+
+        public Vector3 direction 
+        {
+            get { return _direction; }
+            set { _direction = value; }
+        }
+
+        public Vector3 GetPoint(float distance)
+        {
+            return _origin + _direction * distance;
+        }
+    }
+}
+
