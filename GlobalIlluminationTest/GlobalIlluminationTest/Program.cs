561c9017adc62fdf2e1d01313b2007aed0128dfc
wangtianhang <690879430@qq.com>
2018-05-06 17:52:19 +0800

    第三章测试通过

--- GlobalIlluminationTest/GlobalIlluminationTest/Program.cs
+++ GlobalIlluminationTest/GlobalIlluminationTest/Program.cs
@@ -3,36 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using Common;
 
-namespace GlobalIlluminationTest
+/// <summary>
+/// 数学库底层为左手坐标系 camera坐标系为右手坐标系
+/// </summary>
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int nx = 200;
+        int ny = 100;
+        //此数组位于第一象限
+        Color[] data = new Color[nx * ny];
+
+        Vector3 lower_left_corner = new Vector3(-2, -1, -1);
+        Vector3 horizontal = new Vector3(4, 0, 0);
+        Vector3 vertical = new Vector3(0, 2, 0);
+        Vector3 origin = Vector3.zero;
+
+        for (int j = ny - 1; j >= 0; j-- )
         {
-            int nx = 200;
-            int ny = 100;
-            //此数组位于第一象限
-            Color[] data = new Color[nx * ny];
-
-            for (int j = ny - 1; j >= 0; j-- )
+            for (int i = 0; i < nx; ++i )
             {
-                for (int i = 0; i < nx; ++i )
-                {
-                    byte alpha = 255;
-                    byte r = (byte)((float)i / (float)nx * 255);
-                    byte g = (byte)((float)j / (float)ny * 255);
-                    byte b = (byte)(0.2 * 255);
-                    Color color = Color.FromArgb(alpha, r, g, b);
-                    int index = j * nx + i;
-                    data[index] = color;
-                }
+                float u = (float)i / (float)nx;
+                float v = (float)j / (float)ny;
+
+//                 byte alpha = 255;
+//                 byte r = (byte)((float)i / (float)nx * 255);
+//                 byte g = (byte)((float)j / (float)ny * 255);
+//                 byte b = (byte)(0.2 * 255);
+                Ray ray = new Ray(origin, lower_left_corner + u * horizontal + v * vertical);
+                //Color color = Color.FromArgb(255, r, g, b);
+                Color color = ColorLerp(ray);
+
+                int index = j * nx + i;
+                data[index] = color;
             }
-
-            DrawHelper.DrawData("test.bmp", nx, ny, data, "by wth");
-
-            Console.WriteLine("end");
-            Console.ReadLine();
         }
+
+        DrawHelper.DrawData("test.bmp", nx, ny, data, "by wth");
+
+        Console.WriteLine("end");
+        Console.ReadLine();
+    }
+
+    static Color ColorLerp(Ray ray)
+    {
+        Color src = Color.White;
+        Color des = Color.Blue;
+        
+        float y = ray.direction.y;
+        float weight = (y + 1) / 2;
+        //float range = 1 - (-1);
+        //float weight = -1 + offset * range;
+
+        int r = (int)((1 - weight) * src.R + weight * des.R);
+        int g = (int)((1 - weight) * src.G + weight * des.G);
+        int b = (int)((1 - weight) * src.B + weight * des.B);
+
+        return Color.FromArgb(255, r, g, b);
     }
 }
+
