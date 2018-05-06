d6b14da7ff19bac4114ac0547276e5fbb566a719
wangtianhang <690879430@qq.com>
2018-05-06 17:06:23 +0800

    添加左手坐标系数学库

---
+++ GlobalIlluminationTest/GlobalIlluminationTest/Math/Random.cs
@@ -0,0 +1,57 @@
+﻿//using UnityEngine;
+
+namespace Common
+{
+    /// <summary>
+    /// 禁止非战斗逻辑模块使用此随机数
+    /// </summary>
+    public static class Random
+    {
+        static System.Random s_random = null;
+        static int s_seed = 0;
+        public static int s_index = 0;
+
+        public static void SetSeed(int seed)
+        {
+            s_seed = seed;
+            s_random = new System.Random(s_seed);
+        }
+
+        /// <summary>
+        /// 左闭右开区间 与unity一致
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static int Range(int minInclusive, int maxExclusive)
+        {
+            s_index++;
+            if (s_random == null)
+            {
+                s_random = new System.Random(s_seed);
+            }
+
+            int ret = s_random.Next(minInclusive, maxExclusive);
+            return ret;
+        }
+
+        /// <summary>
+        /// 左闭右闭区间 与unity一致
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public static float Range(float minInclusive, float maxInclusive)
+        {
+            s_index++;
+            if (s_random == null)
+            {
+                s_random = new System.Random(s_seed);
+            }
+
+            int randomInteger = s_random.Next(0, int.MaxValue);
+            float randomFloat = (float)randomInteger / (float)int.MaxValue;
+            float range = maxInclusive - minInclusive;
+            float ret = minInclusive + randomFloat * range;
+            return ret;
+        }
+    }
+}
---
+++ GlobalIlluminationTest/GlobalIlluminationTest/Math/RotateHelper.cs
@@ -0,0 +1,29 @@
+﻿//using UnityEngine;
+using System.Collections;
+
+class RotateHelper 
+{
+    // 旋转转朝向
+    public static Common.Vector3 GetForward(Common.Quaternion rotation)
+    {
+        return rotation * Common.Vector3.forward;
+    }
+
+    // 朝向转旋转
+    public static Common.Quaternion LookAt(Common.Vector3 dir)
+    {
+        return Common.Quaternion.LookRotation(dir, Common.Vector3.up);
+    }
+
+#if UNITY_4 || UNITY_5
+    public static Vector3 GetForwardClient(Quaternion rotation)
+    {
+        return rotation * Vector3.forward;
+    }
+
+    public static Quaternion LookAtClient(Vector3 dir)
+    {
+        return Quaternion.LookRotation(dir, Vector3.up);
+    }
+#endif
+}
